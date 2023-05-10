8b4037d7cf4f8f769fed4c1cb700c910bb81430d
Yusup <hunzah9@mail.ru>
2023-05-10 14:55:33 +0300

    add tests

--- src/state/user-reducer.test.ts
+++ src/state/user-reducer.test.ts
@@ -1,22 +1,49 @@
 import {UserReducer, UserType} from './user-reducer';
-const user: UserType = {
-    age: 5,
-    name: 'Vova',
-    childrenCount:2
-}
-test('user should increment only age', () => {
 
-    // const res =
-  UserReducer(user, {type: 'SUM-AGE'})
+test('user should increment only age', () => {
+    const user: UserType = {
+        age: 5,
+        name: 'Vova',
+        childrenCount:2
+    }
+    UserReducer(user, {type: 'SUM-AGE'})
     expect(user.age).toBe(6)
     expect(user.childrenCount).toBe(2)
 
 })
 test('user should increment only childrens', () => {
 
+    const user: UserType = {
+        age: 5,
+        name: 'Vova',
+        childrenCount:2
+    }
+     UserReducer(user, {type: 'ADD-CHILD'})
+    expect(user.age).toBe(5)
+    expect(user.childrenCount).toBe(3)
 
-  UserReducer(user, {type: 'ADD-CHILD'})
+})
+
+test('user should increment only age', () => {
+    const user: UserType = {
+        age: 45,
+        name: 'Ali',
+        childrenCount:5
+    }
+    UserReducer(user, {type: 'Ali-SUM-AGE'})
     expect(user.age).toBe(6)
     expect(user.childrenCount).toBe(2)
+
+})
+test('user should increment only childrens', () => {
+
+    const user: UserType = {
+        age: 5,
+        name: 'Vova',
+        childrenCount:2
+    }
+    UserReducer(user, {type: 'Ali-ADD-CHILD'})
+    expect(user.age).toBe(5)
+    expect(user.childrenCount).toBe(3)
 
 })
--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -9,11 +9,20 @@ export type UserType = {
 
 
 
-export const UserReducer = (state:UserType,action:{type:string}) => {
+export const UserReducer = (state:UserType,action:{type:string}):UserType => {
 switch (action.type) {
     case 'SUM-AGE' :
-        state.age+=1; return
+        return {...state,age:state.age+=1}
     case 'ADD-CHILD':
-        state.childrenCount+=1; return
+        return {...state, childrenCount: state.childrenCount+=1}
     default: return state
 }}
+
+export const UserReducer = (state:UserType,action:{type:string}):UserType => {
+    switch (action.type) {
+        case 'SUM-AGE' :
+            return {...state,age:state.age+=1}
+        case 'ADD-CHILD':
+            return {...state, childrenCount: state.childrenCount+=1}
+        default: return state
+    }}
