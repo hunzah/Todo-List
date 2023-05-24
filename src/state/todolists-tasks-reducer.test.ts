ea90e6db56dfc9762e8e2a3ab50e86880a2e36cb
Yusup <hunzah9@mail.ru>
2023-05-24 21:20:12 +0300

    commit

---
+++ src/state/todolists-tasks-reducer.test.ts
@@ -0,0 +1,16 @@
+test('ids should be equals', () => {
+    const startTasksState: TasksStateType = {};
+    const startTodolistsState: Array<TodolistType> = [];
+
+    const action = AddTodolistAC("new todolist");
+
+    const endTasksState = tasksReducer(startTasksState, action)
+    const endTodolistsState = todolistsReducer(startTodolistsState, action)
+
+    const keys = Object.keys(endTasksState);
+    const idFromTasks = keys[0];
+    const idFromTodolists = endTodolistsState[0].id;
+
+    expect(idFromTasks).toBe(action.todolistId);
+    expect(idFromTodolists).toBe(action.todolistId);
+});
