242a3db4290a810692e6a44515b518d2dc6974ce
Yusup <hunzah9@mail.ru>
2023-06-02 15:54:01 +0300

    code refactoring

--- src/TodoList/tasks-reducer.ts
+++ src/TodoList/tasks-reducer.ts
@@ -18,17 +18,16 @@ export type Action3Type = {
     type: 'CHANGE-TASK-STATUS'
     id: string
     todoListId: string
-    isDone:boolean
+    isDone: boolean
 }
 export type Action4Type = {
     type: 'CHANGE-TASK-TITLE'
     id: string
     todoListId: string
-    newTitle:string
+    newTitle: string
 }
 
 type ActionTypes = Action1Type | Action2Type | Action3Type | Action4Type | AddTodoActionType | RemoveTodoActionType
-
 
 
 const initialState =
@@ -45,7 +44,6 @@ const initialState =
     }
 
 
-
 export const tasksReducer = (state: TasksStateType = initialState, action: ActionTypes): TasksStateType => {
 
     switch (action.type) {
@@ -58,11 +56,12 @@ export const tasksReducer = (state: TasksStateType = initialState, action: Actio
             debugger
             return {...state, [action.todoListId]: [...state[action.todoListId], action.newTask]}
         case 'CHANGE-TASK-STATUS':
-            const copyState = {...state}
-            const tasks = copyState[action.todoListId];
-            const task = tasks.find(t => t.id === action.id);
-            if (task) task.isDone = action.isDone
-            return copyState
+            return {...state,
+                [action.todoListId]: [...state[action.todoListId].map(task => task.id === action.id ? {
+                    ...task,
+                    isDone: action.isDone
+                } : task)]
+            }
         case 'CHANGE-TASK-TITLE':
             const copy = {...state}
             const tasks1 = copy[action.todoListId];
@@ -70,11 +69,11 @@ export const tasksReducer = (state: TasksStateType = initialState, action: Actio
             if (task1) task1.title = action.newTitle
             return copy
         case'ADD-TODO': {
-            return {...state, [action.todolistId]:[]}
+            return {...state, [action.todolistId]: []}
         }
         case'REMOVE-TODO': {
-             const copy = {...state}
-             delete copy[action.id]
+            const copy = {...state}
+            delete copy[action.id]
             return copy
         }
         default:
@@ -92,9 +91,9 @@ export const addTaskAC = (newTask: TaskType, todoListId: string): Action2Type =>
     return {type: 'ADD-TASK', todoListId: todoListId, newTask: newTask} as const
 }
 
-export const changeTaskStatusAC = (id: string, todoListId: string, isDone:boolean): Action3Type => {
-    return {type: 'CHANGE-TASK-STATUS', todoListId: todoListId, id: id, isDone:isDone} as const
+export const changeTaskStatusAC = (id: string, todoListId: string, isDone: boolean): Action3Type => {
+    return {type: 'CHANGE-TASK-STATUS', todoListId: todoListId, id: id, isDone: isDone} as const
 }
-export const changeTaskTitleAC = (id: string, todoListId: string, newTitle:string): Action4Type => {
-    return {type: 'CHANGE-TASK-TITLE', todoListId: todoListId, id: id, newTitle:newTitle} as const
+export const changeTaskTitleAC = (id: string, todoListId: string, newTitle: string): Action4Type => {
+    return {type: 'CHANGE-TASK-TITLE', todoListId: todoListId, id: id, newTitle: newTitle} as const
 }
