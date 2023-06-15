c374a0bd9c0c483e7dbf8d994a305dd11547c86b
Yusup <hunzah9@mail.ru>
2023-06-15 15:32:15 +0300

    add snapshots and tests

--- integration/setupTests.js
+++ integration/setupTests.js
@@ -1,6 +1,3 @@
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
 
 expect.extend({ toMatchImageSnapshot });
-const { toMatchImageSnapshot } = require('jest-image-snapshot');
-
-expect.extend({ toMatchImageSnapshot });
