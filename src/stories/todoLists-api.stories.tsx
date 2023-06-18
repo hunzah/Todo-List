641b60b6faff17b81bfed1d347e961c425d99f8d
Yusup <hunzah9@mail.ru>
2023-06-18 06:55:15 +0300

    commit

--- src/stories/todoLists-api.stories.tsx
+++ src/stories/todoLists-api.stories.tsx
@@ -3,19 +3,37 @@ import {useEffect, useState} from 'react';
 import axios from 'axios';
 
 const meta: Meta = {
-    title: 'Example/Api',
+    title: 'Example/Get/Post/Delete/Put,',
 };
+
+const settings = {
+    withCredentials: true,
+    headers: {
+        'API-KEY': '6d618c5c-24f1-48e7-9694-a9a2e7863199'
+    }
+}
 
 export default meta;
 
 const getTodoLists = () => {
     const [state, setState] = useState({name: 'Bob'})
     useEffect(() => {
-        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists/{todolistId}')
-            .then((res)=>{
-                setState(res)
-        })
-    },[]
-)
+            axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists/{todolistId}', settings)
+                .then((res) => {
+                    setState(res.data)
+                })
+        }, []
+    )
+    return <div>{JSON.stringify(state)}</div>
+}
+const postTodoLists = () => {
+    const [state, setState] = useState({name: 'Bob'})
+    useEffect(() => {
+            axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists/{todolistId}', {title: 'Заголовок'}, settings)
+                .then((res) => {
+                    setState(res.data)
+                })
+        }, []
+    )
     return <div>{JSON.stringify(state)}</div>
 }
