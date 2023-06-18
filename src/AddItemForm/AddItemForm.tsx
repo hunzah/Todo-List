3e543b1f148bc1581f147662a9442e02a06648e2
Yusup <hunzah9@mail.ru>
2023-06-18 16:09:56 +0300

    add custom hooks

--- src/AddItemForm/AddItemForm.tsx
+++ src/AddItemForm/AddItemForm.tsx
@@ -3,49 +3,19 @@ import s from './AddItem.module.css'
 import {TextField} from '@mui/material';
 import ControlPoint from '@mui/icons-material/ControlPoint';
 import IconButton from '@mui/material/IconButton';
-
-type AddItemFormPropsType = {
-    addItem: (title: string) => void
-    // titleForButtons: string
-}
+import {AddItemFormPropsType, useAddItemForm} from './hooks/useAddItemForm';
 
 
-export const AddItemForm = React.memo((props: AddItemFormPropsType)=> {
-    console.log('item form was called')
-    const [newItemTitle, setNewItemTitle] = useState('')
-    const [error, setError] = useState<string | null>(null)
+export const AddItemForm = React.memo((addItem: AddItemFormPropsType) => {
 
-
-    const onNewTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setNewItemTitle(e.currentTarget.value)
-    }
-
-
-    const isAddTaskNotPossible = newItemTitle.length === 0 || newItemTitle.trim().length > 20
-    const onKeyPressHandler =
-        isAddTaskNotPossible ? undefined
-            : (e: KeyboardEvent<HTMLInputElement>) => {
-                if (e.key === 'Enter') {
-                    if (newItemTitle.trim() === '') {
-                        setError('Title is required')
-                    } else {
-                        props.addItem(newItemTitle.trim())
-                        setNewItemTitle('')
-                        setError(null)
-                    }
-                }
-            }
-
-
-    const onClickHandler = () => {
-        if (newItemTitle.trim() === '') {
-            setError('Title is required')
-        } else {
-            props.addItem(newItemTitle.trim())
-            setNewItemTitle('')
-        }
-    }
-
+    const {
+        newItemTitle,
+        error,
+        onNewTitleChangeHandler,
+        onKeyPressHandler,
+        onClickHandler,
+        isAddTaskNotPossible
+    } = useAddItemForm(addItem)
     return (
         <>
             <div className={s.inpBtn}>
